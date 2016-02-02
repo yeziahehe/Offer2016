1d6c6df9c8e133ecb82c77c1c926ac0cbc3a4e2d
叶帆 <yeziahehe@gmail.com>
2016-02-02 17:57:26 +0800

    first commit

---
+++ 01_AssignmentOperator/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  01_AssignmentOperator
+//
+//  Created by 叶帆 on 16/2/2.
+//  Copyright © 2016年 Suzhou Coryphaei Information&Technology Co., Ltd. All rights reserved.
+//
+
+/**
+ *  题目: 如下为类型CMyString的声明，请为该类型添加赋值运算符函数。
+    class CMyString
+    {
+    public:
+        CMyString(char* pData = NULL);
+        CMyString(const CMyString& str);
+        ~CMyString(void);
+ 
+    private:
+        char *m_pData;
+    };
+ *
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
